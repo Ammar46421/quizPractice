94cb84111e29357b08d7e86d9be57db7403255ab
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-11-15 15:59:12 +0500

    Array

---
+++ app.js
@@ -0,0 +1,20 @@
+// Arrays Practice starts
+
+// var cities = [];
+// cities.push('karachi', 'islamabad', 'lahore');
+// cities.pop();
+// cities.shift();
+// cities.unshift('kk')
+// console.log(cities);
+// var enterCity = prompt('Enter city').toLowerCase();
+
+// if (cities.includes(enterCity) ) {
+//     alert('Right Answer')
+// }else{
+//     alert('Wrong Answer')
+// };
+
+// Arrays Practice End
+
+// for loop practice start
+
